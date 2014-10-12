41931d4b6704708a10717629a592f9dc6f9545ea
Yann Gouffon <yago@yago-art.ch>
2014-10-12 12:04:33 +0200

    add show database schema based on Step 4

--- server.js
+++ server.js
@@ -6,6 +6,31 @@ var express = require('express'),
     mongoose = require('mongoose'),
     bcrypt = require('bcryptjs');
 
+var showSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  airsDayOfWeek: String,
+  airsTime: String,
+  firstAired: Date,
+  genre: [String],
+  network: String,
+  overview: String,
+  rating: Number,
+  ratingCount: Number,
+  status: String,
+  poster: String,
+  subscribers: [{
+    type: mongoose.Schema.Types.ObjectId, ref: 'User'
+  }],
+  episodes: [{
+      season: Number,
+      episodeNumber: Number,
+      episodeName: String,
+      firstAired: Date,
+      overview: String
+  }]
+});
+
 var app = express();
 
 app.set('port', process.env.PORT || 9000);
